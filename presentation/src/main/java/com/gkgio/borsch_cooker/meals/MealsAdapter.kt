be678783261600313ae138fd062e3253a964b0ca
Danila Karpyzin <karp123@inbox.ru>
2020-08-09 19:03:02 +0300

    release
    1. Сверстал лэйаут страницы
    2. Добавил новые стринги
    3. Создал адаптер для блюд
    
    Примечание: оставил мок для просмотра блюд

---
+++ presentation/src/main/java/com/gkgio/borsch_cooker/meals/MealsAdapter.kt
@@ -0,0 +1,43 @@
+package com.gkgio.borsch_cooker.meals
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.gkgio.borsch_cooker.R
+import com.gkgio.borsch_cooker.ext.setDebounceOnClickListener
+import com.gkgio.borsch_cooker.ext.withCenterCropRoundedCorners
+import com.gkgio.borsch_cooker.view.SyntheticViewHolder
+import kotlinx.android.synthetic.main.layout_meals_view_holder.view.*
+
+class MealsAdapter(
+    val itemClick: (position: Int) -> Unit
+) : RecyclerView.Adapter<SyntheticViewHolder>() {
+
+    private var mealsList = listOf<MealsItemUi>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SyntheticViewHolder {
+        return SyntheticViewHolder.inflateFrom(parent, R.layout.layout_meals_view_holder)
+    }
+
+    override fun getItemCount(): Int = mealsList.size
+
+    override fun onBindViewHolder(holder: SyntheticViewHolder, position: Int) =
+        with(holder.itemView) {
+            mealsEdit.setDebounceOnClickListener {
+                itemClick(mealsList[position].id)
+            }
+            Glide.with(mealsImage)
+                .load(mealsList[position].image)
+                .placeholder(R.drawable.ic_image_placeholder)
+                .withCenterCropRoundedCorners(context, 8)
+                .into(mealsImage)
+            mealsItemName.text = mealsList[position].name
+            mealsPrice.text = context.getString(R.string.orders_sum, mealsList[position].price)
+        }
+
+    fun setMealsList(mealsList: List<MealsItemUi>) {
+        this.mealsList = mealsList
+        notifyDataSetChanged()
+    }
+
+}
---
+++ presentation/src/main/java/com/gkgio/borsch_cooker/meals/MealsItemUi.kt
@@ -0,0 +1,8 @@
+package com.gkgio.borsch_cooker.meals
+
+data class MealsItemUi(
+    val id: Int,
+    val name: String,
+    val image: String,
+    val price: Int
+)
